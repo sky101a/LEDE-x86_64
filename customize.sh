a49a55348cbe644fc399f7de4e8264f3a8b06f3f
sky101a <65938943+sky101a@users.noreply.github.com>
2020-12-11 09:40:24 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,5 +12,3 @@ sed -i 's/192.168.1.1/192.168.31.189/g' openwrt/package/base-files/files/bin/con
 #cp -f package/litte/banner package/base-files/files/etc/banner
 #cp -f package/litte/Leandiffconfig diffconfig && cp diffconfig .config && make defconfig
 #./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
