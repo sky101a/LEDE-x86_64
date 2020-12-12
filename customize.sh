079120f64cf3448d3e530eb07b301b82bd5223e9
sky101a <65938943+sky101a@users.noreply.github.com>
2020-12-12 23:10:31 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,6 +6,8 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.31.189/g' openwrt/package/base-files/files/bin/config_generate
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 #2. 自定义设置
 #cp -f package/litte/default-settings package/lean/default-settings/files/zzz-default-settings
